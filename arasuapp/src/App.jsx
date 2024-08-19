53b70068a6f2507533917f3629860a5a2e38f10b
Arasu-j <arasuthedon03@gmail.com>
2024-08-19 19:36:07 +0530

    Added Header and Footer components and updated App.jsx

--- arasuapp/src/App.jsx
+++ arasuapp/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Header from './components/header';
+import Footer from './components/Footer';
 import LoginForm from './components/loginform';
 
 function App() {
   return (
     <div>
-      <h1>Login</h1>
-      <LoginForm />
+      <Header />
+      <main>
+        <LoginForm />
+      </main>
+      <Footer />
     </div>
   );
 }
