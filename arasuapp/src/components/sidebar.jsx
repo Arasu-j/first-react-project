53868b77ce2bd122ce194f7d41ff8c06d2af4bc5
Arasu-j <arasuthedon03@gmail.com>
2024-08-20 22:09:42 +0530

    added sidebar

---
+++ arasuapp/src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Sidebar</h2>
+      <p>This is the sidebar content.</p>
+    </aside>
+  );
+}
+
+export default Sidebar;
