0c350e8de977c11bc15abfa7863038dc5484052b
Arasu-j <arasuthedon03@gmail.com>
2024-08-20 22:12:21 +0530

    updated

--- arasuapp/src/App.jsx
+++ arasuapp/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/Footer';
-import LoginForm from './components/loginform';
+import LoginForm from './components/LoginForm';
+import Sidebar from './components/sidebar';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-      <main>
-        <LoginForm />
-      </main>
+      <div className="app-layout">
+        <Sidebar /> 
+        <main className="main-content">
+          <LoginForm />
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
