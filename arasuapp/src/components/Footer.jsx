abf0cfb6f03208b83bc89809f66d7e1d3f1f934e
Arasu-j <arasuthedon03@gmail.com>
2024-08-21 22:36:05 +0530

    updated

--- arasuapp/src/components/Footer.jsx
+++ arasuapp/src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 My Fullstack Project. All rights reserved.</p>
+      <p>&copy; 2024 My Fullstack Project. All rights reserved.
+        for more details contact
+      </p>
+      
     </footer>
   );
 };
