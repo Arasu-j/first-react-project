59e88fd053f8f80be36a4901f6bac35578b1f637
Arasu-j <arasuthedon03@gmail.com>
2024-08-25 22:08:56 +0530

    small changes

--- arasuapp/src/App.jsx
+++ arasuapp/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import Footer from './components/Footer';
-import LoginForm from './components/LoginForm';
+import LoginForm from './components/loginform';
 import Sidebar from './components/sidebar';
 
 function App() {
